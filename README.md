094f5c5d55fe04757f43522658248d62b78ff780
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2015-10-14 07:19:39 -0400

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Binary Search Tree D3
+
+Shows how binary search trees works.
---
+++ views/bst.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * Created by yusaira-khan on 14/10/15.
+ */
+
+console.log('hello World!');
+//CONSTANTS
+var svgWidth = 620, svgHeight = 620;
+var margin = {top: 10, bottom: 10, right: 10, left: 10};
+var transitionDuration = 300;
+var circle_radius = 20;
+var svg = d3.select('#tree').append('svg').attr({
+  'width': svgWidth,
+  'height': svgHeight
+});
+var chartWidth = svgWidth - margin.right - margin.left, chartHeight = svgHeight - margin.top - margin.bottom;
+
+$new = $('#new');
+$('#enter').on('click',function enterNew(){
+  var num = parseInt($new.val());
+  var pos=insertInTree(num);
+var g=svg.append('g');
+  g.append('circle').attr({
+
+            });
+
+
+});
