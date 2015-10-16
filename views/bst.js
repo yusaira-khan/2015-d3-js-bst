fe1e96321ca33b805dc76222103a4af8ebd0ced2
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2015-10-15 22:20:39 -0400

    adding resizing

--- views/bst.js
+++ views/bst.js
@@ -5,34 +5,45 @@
 
 console.log('hello World!');
 //CONSTANTS
-var svgWidth = 620, svgHeight = 620;
+var minSide = 500;
 var margin = {top: 10, bottom: 10, right: 10, left: 10};
 var transitionDuration = 300;
 var circle_radius = 20;
 var minAngleDegrees = 20;
-var svg = d3.select('#tree').append('svg').attr({
-  'width': svgWidth,
-  'height': svgHeight
-});
-var chartWidth = svgWidth - margin.right - margin.left, chartHeight = svgHeight - margin.top - margin.bottom;
+
+var svg;
+function resize_svg(height) {
+  var svgWidth = minSide, svgHeight=minSide;
+  if(height){
+    svgHeight=Math.max(minside, height * 100);
+    svgWidth= Math.max(minside,height*100);
+  }
+  svg = d3.select('#tree').append('svg').attr({
+    'width': svgWidth,
+    'height': svgHeight
+  });
+}
+resize_svg();
 var treeToot = null;
 
 $new = $('#new');
 $('#enter').on('click', function enterNew() {
   var num = parseInt($new.val());
   addToTree(num);
-  var h=treeToot.height();
-
+  var h = treeToot.height();
+  clearCircles();
+  resize_svg(h);
 });
 
 function clearCircles() {
-  svg.selectAll('g').remove();
+//  svg.selectAll('g').remove();
+  svg.remove();
 }
 
-function addToTree(num){
-  if(!treeToot){
-    treeToot=new TreeNode({tkey:num});
-  }else{
+function addToTree(num) {
+  if (!treeToot) {
+    treeToot = new TreeNode({tkey: num});
+  } else {
     treeToot.insert(num);
   }
 }
