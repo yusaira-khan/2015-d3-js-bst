03876668ac2c8dcd1847d8087b7a51dec55b0529
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2015-10-15 21:44:05 -0400

    height function

--- public/javascripts/tree.js
+++ public/javascripts/tree.js
@@ -27,26 +27,44 @@ TreeNode.prototype.insert = function (tkey) {
     }
   }
 };
+
+Tree.prototype.height = function () {
+  if (this.left == null && this.right == null) {
+    this.h = 1;
+  }
+  else if (this.left == null) {
+    this.h = this.right.height() + 1;
+  } else if (this.right == null) {
+    this.h = this.right.height() + 1;
+  }
+  else {
+    this.h = Math.max(this.right.height(), this.left.height()) + 1;
+
+  }
+  return this.h
+};
+
 TreeNode.prototype.min = function () {
   if (this.left == null)
     return this;
   else
     return this.left.min()
 };
+
 TreeNode.prototype.replaceChild = function (newChild) {
   if (this.parent.left == this) {
     this.parent.left = newChild;
   }
-  else if (this.parent.right == this){
-
+  else if (this.parent.right == this) {
+    this.parent.right = newChild;
   }
 
 };
+
 TreeNode.prototype.delete = function (tkey) {
   if (tkey == this.data.tkey) {
-    if (this.left && this.right){
+    if (this.left && this.right) {
       var min = this.right.min();
     }
   }
-}
-;
+};
